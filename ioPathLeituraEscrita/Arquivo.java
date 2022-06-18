7e3f413ce23644c1a6fb90f55aefb5e2884f615d
juvenciolinhares <jlinhares@cpqi.com>
2022-06-18 15:20:23 -0300

    io Path Leitura Escrita atualizacao

--- ioPathLeituraEscrita/Arquivo.java
+++ ioPathLeituraEscrita/Arquivo.java
@@ -7,33 +7,26 @@ import java.nio.file.Paths;
 
 public class Arquivo {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException  {
+	
+		Path path = Paths.get("C:\\ws-eclipse\\cursoJava2\\files\\texto.txt");//inicio de tudo: classe path=> define/manipula os caminhos 
+		System.out.println(path.toAbsolutePath());//retorna o caminho absoluto
+		System.out.println(path.getParent());//arquivo pai desse arquivo.
+		System.out.println(path.getRoot());//raiz
+		System.out.println(path.getFileName());//nome do arquivo
 		
-		Path path = Paths.get("C:\\ws-eclipse\\cursoJava2");//recebe uma string pra ser informado a localização do arquivo/diretório a ser manipulado
-		
-		path.toAbsolutePath();//retorna o caminho absoluto do arquivo
-		System.out.println(path.toAbsolutePath());
-		
-		path.getParent();//arquivo pai
-		System.out.println(path.getParent());
-		
-		path.getRoot();//raíz do sistema
-		System.out.println(path.getRoot());
-		
-		path.getFileName();//nome do arquivo
-		System.out.println(path.getFileName());
-		
-		//Criação de diretrórios p/ armazenar arquivos:
-		
-
+		//CRIAÇÃO DE DIRETÓRIOS:(até o caminho java2: pai do arquivo)
+		Files.createDirectories(path.getParent());
 		
 		//escrever e ler arquivos:
 		
-		byte[] bytes = "Meu texto".getBytes();
-		Files.write(path, bytes);//cria, limpa e escreve
+		byte [] bytes = "Meu Texto".getBytes();
+		Files.write(path, bytes);// cria, limpa, escreve;
 		
+		//ler um arquivo:
 		byte [] retorno = Files.readAllBytes(path);
-		System.out.println(retorno);
+		System.out.println(new String (retorno));
+		
 
 	}
 
